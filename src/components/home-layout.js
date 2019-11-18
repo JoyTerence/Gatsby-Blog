6a968394bc712e31a6c343fdad269b82523f1308
barnes4 <barnes4@avaya.com>
2019-11-18 12:15:29 +0530

    Don't show home link when in home page

--- src/components/home-layout.js
+++ src/components/home-layout.js
@@ -8,11 +8,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql, Link} from "gatsby"
+import { Location } from '@reach/router';
 
 import "./homeLayout.css"
-
-import { MdHome } from "react-icons/md";
-import { IconContext } from "react-icons";
 
 const HomeLayout = ({ children }) => (
   <StaticQuery
@@ -32,13 +30,24 @@ const HomeLayout = ({ children }) => (
           {children} 
         </div>
         <footer>
-          <Link to="/" target="_blank">
-            <div className="home-card">
-              <IconContext.Provider value={{color:`#0077b5`, size: `3em`}}>
-                < MdHome />
-              </IconContext.Provider>
-            </div>
-          </Link>
+          <Location>
+            {
+              ({ location }) => {
+                var showhomebutton = location.pathname != '/'
+                console.log(showhomebutton)
+                return showhomebutton? 
+                  <Link to="/" target="_blank">
+                    <div className="home-card">
+                      <div className="home-nav">
+                        <IconContext.Provider value={{color:`#0077b5`, size: `2em`}}>
+                          < MdHome />
+                        </IconContext.Provider>
+                      </div>
+                    </div>
+                  </Link> : <div></div>
+              }
+            }
+          </Location>
         </footer>
       </>
     )}
