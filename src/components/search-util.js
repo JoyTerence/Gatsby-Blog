9b54973d5b424bef05dc7bf07ba3dc6596580e77
barnes4 <barnes4@avaya.com>
2019-11-22 16:49:58 +0530

    Search irrespective of case

--- src/components/search-util.js
+++ src/components/search-util.js
@@ -4,6 +4,10 @@ const searcher = ( data, tags ) => {
         if (elem.node.fields.slug !== "/aboutme/") {
             var subjectArray = elem.node.frontmatter.tags;
             var searchKeyArray = tags;
+
+            subjectArray = subjectArray.map(function(x){return x.toUpperCase()})
+            searchKeyArray = searchKeyArray.map(function(x){return x.toUpperCase()})
+
             var found = searchKeyArray.some(r => subjectArray.indexOf(r) >= 0)
             if (found) {
                 return true
